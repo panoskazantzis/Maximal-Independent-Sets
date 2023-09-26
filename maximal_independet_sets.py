a4708e5e26824f53ca4dcb862699b04241153cf9
Panos Kazantzis <144696161+panoskazantzis@users.noreply.github.com>
2023-09-26 08:44:36 +0100

    Add files via upload

--- maximal_independet_sets.py
+++ maximal_independet_sets.py
@@ -41,3 +41,68 @@ def get_adjacency_matrix(graph, nodes):
         adj_mtrx[i, i] = 1
         
     return adj_mtrx
+
+
+def maximal_independent_sets(graph, strategy='min degree'):
+    # calculate adjacency matrix
+    adj_matrix = get_adjacency_matrix(graph, np.unique(graph.flatten()))
+    # find 1st independent maximal set
+    
+    
+    max_ind_sets = []
+    
+    # get nodes degree
+    track_nodes = list(np.unique(graph.flatten()))
+    nodes_degree = {
+        track_nodes[i]: np.sum(adj_matrix, axis=1)[i]
+        for i in range(0, len(track_nodes))
+    }
+    # get all disconnected nodes to each node
+    disconnected_nodes = {
+        node: np.where(adj_matrix[node] == 0)[0]
+        for node in track_nodes
+    }
+    
+    max_ind_sets = {}
+    while len(nodes_degree) > 0:
+    
+        # prioritize
+        if strategy == 'min degree':
+            priority_node = min(nodes_degree, key=nodes_degree.get)
+        elif strategy == 'max degree':
+            priority_node = max(nodes_degree, key=nodes_degree.get)
+    
+        # add to set
+        max_ind_set = [priority_node]
+        max_ind_sets[priority_node] = []
+        
+        
+        while len(max_ind_set) > 0:
+            #print(max_ind_set)
+            #
+            # get all accepted nodes
+            bad_nodes = [
+                node for node in set(np.unique(graph.flatten()))
+                for in_node in max_ind_set
+                if node not in disconnected_nodes[in_node] or node in set(max_ind_set) 
+                or any(set(max_ind_set+[node]).issubset(set(mis)) for mis in max_ind_sets[priority_node])
+                #or max_ind_set+[node] in max_ind_sets
+            ]
+
+            accepted_nodes = set(np.unique(graph.flatten())) - set(bad_nodes)
+            if len(accepted_nodes) > 0:
+                max_ind_set += [list(accepted_nodes)[0]]
+            else:
+                # if subset do not include
+                if not any(set(max_ind_set).issubset(set(mis)) for mis in max_ind_sets[priority_node]):
+                    max_ind_sets[priority_node] += [max_ind_set]
+                max_ind_set = max_ind_set[:-1]
+            
+        # next starting node
+        nodes_degree = {item[0]: item[1] for item in nodes_degree.items() if item[0] != priority_node}
+    
+    max_ind_sets = [
+        list(set(mis)) for mis in list(chain.from_iterable(list(max_ind_sets.values())))
+    ]
+    
+    return list(set(map(tuple, max_ind_sets)))
